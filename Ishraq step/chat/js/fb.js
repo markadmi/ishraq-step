5621016f19b965c1a68df9982fad7fadeaa036cc
markadmi <126561046+markadmi@users.noreply.github.com>
2025-10-10 11:38:44 +0300

    Add files via upload

---
+++ Ishraq step/chat/js/fb.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCzokitueX9Ph_LNUekRpG0MZxpPkgcEiQ",
+  authDomain: "hacker-withdraw-photos.firebaseapp.com",
+  databaseURL: "https://hacker-withdraw-photos-default-rtdb.firebaseio.com",
+  projectId: "hacker-withdraw-photos",
+  storageBucket: "hacker-withdraw-photos.appspot.com",
+  messagingSenderId: "295892241734",
+  appId: "1:295892241734:web:01b7841226bc8b189b2947",
+  measurementId: "G-QE2VCZY3NX"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
+export const storage = getStorage(app);
